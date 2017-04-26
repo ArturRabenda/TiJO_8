0c657b212fefe45cdeea3f351e1ce2e00b74fb56
Artur Rabenda <rabenda27178@student.pwsztar.edu.pl>
2017-04-26 12:38:24 +0200

    raport1

--- test/app.spec.js
+++ test/app.spec.js
@@ -5,15 +5,43 @@ var app = require('../app/app');
 
 describe('reverseNumber', function ()
 {
+    it('return reversed numbers ', function () {
+        expect(app.reverseNumber(881436389)).to.eql(983634188);
+    });
 
+
+    /*it('is not number', function()
+     {
+     expect(app.reverseNumber('ddd3')).to.eql(false);
+     });*/
 });
 
 describe('returnOnlyLetter', function ()
 {
-
+    it('return  letters', function()
+    {
+        expect(app.returnOnlyLetter(881436389)).to.eql(false);
+    });
+    /*
+     it('return  letters', function()
+     {
+     expect(app.returnOnlyLetter('zxcv')).to.eql('zxcv');
+     });
+     it('return  letters', function()
+     {
+     expect(app.returnOnlyLetter('zxc222')).to.eql('zxc');
+     });*/
 });
 
 describe('isEmail', function ()
 {
-
+    it('return checked email true', function(){
+        expect(app.isEmail("smithhero1994@o2.pl")).to.eql(true);
+    });
+    /*it('return checked email false', function(){
+     expect(app.isEmail("smithhero1994.pl")).to.eql(false);
+     });
+     it('return checked email false', function(){
+     expect(app.isEmail("smithhero1994@pl")).to.eql(false);
+     });*/
 });
